b7dce598314e9ca8826bc675d6c72d98c0544b64
ElementaryLewis <ElementaryLewis@users.noreply.github.com>
2024-05-27 09:32:33 +0000

    Commit from GitHub Actions (Tests & Linting)

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -7,30 +7,30 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class ProfileUpdateRequest extends FormRequest
 {
-	/**
-	 * Get the validation rules that apply to the request.
-	 *
-	 * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
-	 */
-	public function rules(): array
-	{
-		return [
-			'firstname' => ['required', 'string', 'max:255'],
-			'surname' => ['required', 'string', 'max:255'],
-			'email' => [
-				'required',
-				'string',
-				'lowercase',
-				'email',
-				'max:255',
-				function ($attribute, $value, $fail) {
-					// Check if the email is already in use by another user
-					$user = User::where('email', $value)->first();
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'firstname' => ['required', 'string', 'max:255'],
+            'surname' => ['required', 'string', 'max:255'],
+            'email' => [
+                'required',
+                'string',
+                'lowercase',
+                'email',
+                'max:255',
+                function ($attribute, $value, $fail) {
+                    // Check if the email is already in use by another user
+                    $user = User::where('email', $value)->first();
 
-					// If the email is found and it doesn't belong to the current user or the user being updated, fail the validation
-		
-				},
-			],
-		];
-	}
+                    // If the email is found and it doesn't belong to the current user or the user being updated, fail the validation
+
+                },
+            ],
+        ];
+    }
 }
